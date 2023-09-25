53875acebf98a11df538b9a54071030f1ec3f96c
Erin Shiffler <erinmaeshiffler@gmail.com>
2023-09-25 13:31:10 -0600

    w2d1 lunch

---
+++ journal/02/app.js
@@ -0,0 +1,23 @@
+console.log('javascript is working!')
+
+let firstName = 'Erin'
+
+// SECTION global variables
+const secretMessage = `🧀`
+// !SECTION global variables end
+
+// SECTION functions
+
+// NOTE definition of the function
+function selectCheese() {
+  console.log('Selected Cheese!')
+}
+// NOTE calls the function, tells the function to run
+selectCheese()
+
+// const weirdFunction = () => {
+
+// } this is another option to write a function
+// !SECTION functions end  
+
+
